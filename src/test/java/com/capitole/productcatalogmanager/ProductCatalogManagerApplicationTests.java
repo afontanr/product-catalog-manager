42b8f66955e28ed3aa2e1c50fcd3c8638aa7c875
Alfonso Fontan <alfonso.fontan@the-cocktail.com>
2025-02-15 21:52:41 +0100

    Initial commit

---
+++ src/test/java/com/capitole/productcatalogmanager/ProductCatalogManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitole.productcatalogmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
