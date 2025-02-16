3a5a6042bfa1bf6f27fb33231e37d84c0951caf6
Alfonso Fontan <alfonso.fontan@the-cocktail.com>
2025-02-16 20:03:29 +0100

    add exception handler, class to get properties and initializa datasource and flyway

---
+++ src/main/java/com/capitole/productcatalogmanager/infrastructure/config/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.capitole.productcatalogmanager.infrastructure.config;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+}
